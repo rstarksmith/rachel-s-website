334c2f30c00259c62c839c4961b1227bea2520b7
Rachel <rstark.smith@gmail.com>
2022-02-27 10:23:13 -0600

    style.css updates

--- script.js
+++ script.js
@@ -1,4 +1,19 @@
 const h2 = document.createElement("h2");
-h2.textContent = "This content added by JavaScript";
+h2.textContent = "reading | walking dogs | rollerskating | coding | DIY projects";
 
 document.querySelector("body").appendChild(h2);
+
+
+//const input = document.getElementById('input');
+
+//function clickAlert(button) {
+ // alert('I was clicked!');
+//}
+
+//input.addEventListener('click', clickAlert);
+
+//const button = document.querySelector('button');
+
+//button.addEventListener('click', event => {
+// button.textContent = `Click count: ${event.detail}`;
+//});
