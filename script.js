b9e1e70dd4adf696fc0fd995f4442c973c0fb3ef
Rachel <rstark.smith@gmail.com>
2022-03-04 12:14:28 -0500

    css color changes

--- script.js
+++ script.js
@@ -1,19 +1,4 @@
 const h2 = document.createElement("h2");
-h2.textContent = "reading | walking dogs | rollerskating | coding | DIY projects";
+h2.textContent = "reading | interior design | rollerskating | coding | DIY projects";
 
 document.querySelector("body").appendChild(h2);
-
-
-//const input = document.getElementById('input');
-
-//function clickAlert(button) {
- // alert('I was clicked!');
-//}
-
-//input.addEventListener('click', clickAlert);
-
-//const button = document.querySelector('button');
-
-//button.addEventListener('click', event => {
-// button.textContent = `Click count: ${event.detail}`;
-//});
