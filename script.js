8bbbef4a49f1c58cb1451561a5f7b7227bbf4590
Rachel <rstark.smith@gmail.com>
2022-03-06 09:37:42 -0600

    JS addition

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
+alert("PROJECTS link coming soon!")
+
+
 const h2 = document.createElement("h2");
 h2.textContent = "reading | interior design | rollerskating | coding | DIY projects";
 
 document.querySelector("body").appendChild(h2);
+
+
